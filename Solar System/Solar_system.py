e7cc4f36c2d3f8ce4e348dc566a84374a8b031d1
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-05-22 20:04:56 +0600

    Update Solar_system.py

--- Solar System/Solar_system.py
+++ Solar System/Solar_system.py
@@ -2,7 +2,7 @@ import pygame
 import math
 pygame.init()
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 1000, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solar System Simulation")
 
